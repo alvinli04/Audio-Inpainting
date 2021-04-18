df5cfe02c081debec94da01eb0be9a37e934b787
alvinli04 <alvin.li.j@gmail.com>
2021-04-18 11:15:52 -0400

    working spectogram stuff

--- src/spectrogram.py
+++ src/spectrogram.py
@@ -11,171 +11,27 @@ import multiprocessing
 
 import scipy
 import librosa
-import boto3
-from botocore import UNSIGNED
-from botocore.config import Config
-import requests
+import librosa.display
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-[width, height] = matplotlib.rcParams['figure.figsize']
-if width < 10:
-  matplotlib.rcParams['figure.figsize'] = [width * 2.5, height]
+def plot_waveform(samples, sample_rate):
+    librosa.display.waveplot(samples, sr=sample_rate)
+    plt.show()
 
-np.seterr(divide = 'ignore') 
+def get_mel_spectrogram(samples, sample_rate):
+    sgram = librosa.stft(samples)
+    sgram_mag, _ = librosa.magphase(sgram)
+    mel_scale_sgram = librosa.feature.melspectrogram(S=sgram_mag, sr=sample_rate)
+    mel_sgram = librosa.amplitude_to_db(mel_scale_sgram, ref=np.min)
+    return mel_sgram
 
-def plot_waveform(waveform, sample_rate, title="Waveform", xlim=None, ylim=None):
-  waveform = waveform.numpy()
-
-  num_channels, num_frames = waveform.shape
-  time_axis = torch.arange(0, num_frames) / sample_rate
-
-  figure, axes = plt.subplots(num_channels, 1)
-  if num_channels == 1:
-    axes = [axes]
-  for c in range(num_channels):
-    axes[c].plot(time_axis, waveform[c], linewidth=1)
-    axes[c].grid(True)
-    if num_channels > 1:
-      axes[c].set_ylabel(f'Channel {c+1}')
-    if xlim:
-      axes[c].set_xlim(xlim)
-    if ylim:
-      axes[c].set_ylim(ylim)
-  figure.suptitle(title)
-  plt.show()
-
-def plot_specgram(waveform, sample_rate, title="Spectrogram", xlim=None):
-  waveform = waveform.numpy()
-
-  num_channels, num_frames = waveform.shape
-  time_axis = torch.arange(0, num_frames) / sample_rate
-
-  figure, axes = plt.subplots(num_channels, 1)
-  if num_channels == 1:
-    axes = [axes]
-  for c in range(num_channels):
-    axes[c].specgram(waveform[c], Fs=sample_rate)
-    if num_channels > 1:
-      axes[c].set_ylabel(f'Channel {c+1}')
-    if xlim:
-      axes[c].set_xlim(xlim)
-  figure.suptitle(title)
-  plt.show()
-
-def play_audio(waveform, sample_rate):
-  waveform = waveform.numpy()
-
-  num_channels, num_frames = waveform.shape
-  if num_channels == 1:
-    display(Audio(waveform[0], rate=sample_rate))
-  elif num_channels == 2:
-    display(Audio((waveform[0], waveform[1]), rate=sample_rate))
-  else:
-    raise ValueError("Waveform with more than 2 channels are not supported.")
-
-def plot_spectrogram(spec, title=None, ylabel='freq_bin', aspect='auto', xmax=None):
-  fig, axs = plt.subplots(1, 1)
-  axs.set_title(title or 'Spectrogram (db)')
-  axs.set_ylabel(ylabel)
-  axs.set_xlabel('frame')
-  im = axs.imshow(librosa.power_to_db(spec), origin='lower', aspect=aspect)
-  if xmax:
-    axs.set_xlim((0, xmax))
-  fig.colorbar(im, ax=axs)
-  plt.show()
-
-def plot_mel_fbank(fbank, title=None):
-  fig, axs = plt.subplots(1, 1)
-  axs.set_title(title or 'Filter bank')
-  axs.imshow(fbank, aspect='auto')
-  axs.set_ylabel('frequency bin')
-  axs.set_xlabel('mel bin')
-  plt.show()
-
-def get_spectrogram(
-    n_fft = 400,
-    win_len = None,
-    hop_len = None,
-    power = 2.0,
-):
-  waveform, _ = get_speech_sample()
-  spectrogram = T.Spectrogram(
-      n_fft=n_fft,
-      win_length=win_len,
-      hop_length=hop_len,
-      center=True,
-      pad_mode="reflect",
-      power=power,
-  )
-  return spectrogram(waveform)
-
-def plot_pitch(waveform, sample_rate, pitch):
-  figure, axis = plt.subplots(1, 1)
-  axis.set_title("Pitch Feature")
-  axis.grid(True)
-
-  end_time = waveform.shape[1] / sample_rate
-  time_axis = torch.linspace(0, end_time,  waveform.shape[1])
-  axis.plot(time_axis, waveform[0], linewidth=1, color='gray', alpha=0.3)
-
-  axis2 = axis.twinx()
-  time_axis = torch.linspace(0, end_time, pitch.shape[1])
-  ln2 = axis2.plot(
-      time_axis, pitch[0], linewidth=2, label='Pitch', color='green')
-
-  axis2.legend(loc=0)
-  plt.show()
-
-def plot_kaldi_pitch(waveform, sample_rate, pitch, nfcc):
-  figure, axis = plt.subplots(1, 1)
-  axis.set_title("Kaldi Pitch Feature")
-  axis.grid(True)
-
-  end_time = waveform.shape[1] / sample_rate
-  time_axis = torch.linspace(0, end_time,  waveform.shape[1])
-  axis.plot(time_axis, waveform[0], linewidth=1, color='gray', alpha=0.3)
-
-  time_axis = torch.linspace(0, end_time, pitch.shape[1])
-  ln1 = axis.plot(time_axis, pitch[0], linewidth=2, label='Pitch', color='green')
-  axis.set_ylim((-1.3, 1.3))
-
-  axis2 = axis.twinx()
-  time_axis = torch.linspace(0, end_time, nfcc.shape[1])
-  ln2 = axis2.plot(
-      time_axis, nfcc[0], linewidth=2, label='NFCC', color='blue', linestyle='--')
-
-  lns = ln1 + ln2
-  labels = [l.get_label() for l in lns]
-  axis.legend(lns, labels, loc=0)
-  plt.show()
-
-def print_stats(waveform, sample_rate=None, src=None):
-  if src:
-    print("-" * 10)
-    print("Source:", src)
-    print("-" * 10)
-  if sample_rate:
-    print("Sample Rate:", sample_rate)
-  print("Shape:", tuple(waveform.shape))
-  print("Dtype:", waveform.dtype)
-  print(f" - Max:     {waveform.max().item():6.3f}")
-  print(f" - Min:     {waveform.min().item():6.3f}")
-  print(f" - Mean:    {waveform.mean().item():6.3f}")
-  print(f" - Std Dev: {waveform.std().item():6.3f}")
-  print()
-  print(waveform)
-  print()
+def plot_mel_spectrogram(mel_sgram, sample_rate):
+    librosa.display.specshow(mel_sgram, sr=sample_rate, x_axis='time', y_axis='mel')
+    plt.colorbar(format='%+2.0f dB')
+    plt.show()
 
 if __name__ == '__main__':
-	'''
-	SAMPLE_WAV_SPEECH_URL = "https://pytorch-tutorial-assets.s3.amazonaws.com/VOiCES_devkit/source-16k/train/sp0307/Lab41-SRI-VOiCES-src-sp0307-ch127535-sg0042.wav"
-	with requests.get(SAMPLE_WAV_SPEECH_URL, stream=True) as response:
-  		waveform, sample_rate = torchaudio.load(response.raw)
-	'''
-	waveform, sample_rate = torchaudio.load('../data/sample.wav')
-	print_stats(waveform, sample_rate=sample_rate)
-	plot_waveform(waveform, sample_rate)
-	plot_specgram(waveform, sample_rate)
-	#play_audio(waveform, sample_rate)
+    waveform, sample_rate = librosa.load('../data/sample.wav', sr=None)
+    plot_mel_spectrogram(get_mel_spectrogram(waveform, sample_rate), sample_rate)

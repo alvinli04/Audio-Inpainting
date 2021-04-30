b8bf09765cf2d349783440946cc221aad78cb928
Lucas-Lee11 <lucasclee11@gmail.com>
2021-04-29 20:47:26 -0400

    dividing up audio samples

--- src/train.py
+++ src/train.py
@@ -23,27 +23,33 @@ import model
 
 def main():
     waveform, sample_rate = librosa.load('../data/sample.wav', sr=None)
+
     mspec = sp.get_mel_spectrogram(waveform, sample_rate)
-    mspec = mspec[None, None, :, :128]
+
+    _ , length = mspec.shape
+
+    training_data = []
+    for s in [i * 128 for i in range(length // 128) if i < length]:
+
+        context = mspec[ : , s : s + 96].copy()
+        context = np.pad(context, ((0,0), (0, 32)), 'constant')
+        target = mspec[ : , s + 96 : s + 128]
+
+        training_data.append((context, target))
+
+        sp.plot_mel_spectrogram(context, sample_rate)
+        sp.plot_mel_spectrogram(target, sample_rate)
 
 
-    #print(mspec.shape)
-    #print(mspec)
-    x = torch.from_numpy(mspec)
-
-    #print(x.size())
-
-    m = model.Model()
-
-    # print(m)
+    # mspec = mspec[None, None, :, :128]
     #
-    # params = list(m.parameters())
-    # print(len(params))
-    # print(params[0].size())
-    m.eval()
-    output = m(x)
-    print(output)
-    print(output.shape)
+    # x = torch.from_numpy(mspec)
+    # m = model.Model()
+    #
+    # m.eval()
+    # output = m(x)
+    # print(output)
+    # print(output.shape)
 
 if __name__ == "__main__":
     main()

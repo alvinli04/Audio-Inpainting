943c63b1f426e9af794e9496ef8383ad351ba328
alvinli04 <alvin.li.j@gmail.com>
2021-05-07 09:38:24 -0400

    messed around with mel inverse conversion

--- src/spectrogram.py
+++ src/spectrogram.py
@@ -21,7 +21,8 @@ def plot_waveform(samples, sample_rate):
     plt.show()
 
 def get_mel_spectrogram(samples, sample_rate):
-    sgram = librosa.stft(samples)
+    sgram = librosa.stft(samples, n_fft=512)
+    #mess around with nfft, hopsize (match with mel_to_audio), increase mel bins
     sgram_mag, _ = librosa.magphase(sgram)
     mel_scale_sgram = librosa.feature.melspectrogram(S=sgram_mag, sr=sample_rate)
     mel_sgram = librosa.amplitude_to_db(mel_scale_sgram, ref=np.min)
@@ -33,13 +34,14 @@ def plot_mel_spectrogram(mel_sgram, sample_rate):
     plt.show()
 
 def get_waveform(mel_sgram, sample_rate):
-    mel_sgram = librosa.db_to_amplitude(S_db=mel_sgram, ref=.1e-12)
-    return librosa.feature.inverse.mel_to_audio(M=mel_sgram, sr=sample_rate)
+    mel_sgram = librosa.db_to_amplitude(S_db=mel_sgram, ref=.000001)
+    return librosa.feature.inverse.mel_to_audio(M=mel_sgram, sr=sample_rate, n_fft=512, hop_length=128)
 
 if __name__ == '__main__':
     waveform, sample_rate = librosa.load('../data/sample.wav')
     sg = get_mel_spectrogram(waveform, sample_rate)
     wv = get_waveform(sg, sample_rate)
+    plot_mel_spectrogram(sg, sample_rate)
     plot_waveform(waveform, sample_rate)
     plot_waveform(wv, sample_rate)
     sf.write('../data/sample_out.wav', wv, sample_rate, subtype='PCM_24')
